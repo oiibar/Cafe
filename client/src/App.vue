ffadd9aa31a319a70ba5881b08893a4489e04fd0
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-06-05 17:54:24 +0500

    basic layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,42 @@
 <template>
-  <div>App</div>
+  <div>
+    <header class="flex">
+      <div>Logo</div>
+      <nav class="flex">
+        <div>Home</div>
+        <div>Menu</div>
+        <div>Contact</div>
+      </nav>
+    </header>
+    <body>
+      <div class="menu">
+        <div class="coffee">
+          <img />
+          <h3>Name</h3>
+          <p>Description</p>
+          <p>Price</p>
+          <my-button>Details</my-button>
+        </div>
+        <div>
+          <img />
+          <h3>Name</h3>
+          <p>Description</p>
+          <p>Price</p>
+        </div>
+        <div>
+          <img />
+          <h3>Name</h3>
+          <p>Description</p>
+          <p>Price</p>
+        </div>
+      </div>
+    </body>
+  </div>
 </template>
 
 <script>
+import "./assets/css/tailwind.css";
 export default {};
 </script>
 
-<style scoped></style>
+<style></style>
