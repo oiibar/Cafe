d7580ac0b8d7a1a376f2e5924443f4f940486198
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-06-12 14:35:03 +0500

    slider

--- client/src/components/Review.vue
+++ client/src/components/Review.vue
@@ -5,29 +5,41 @@
     >
       <h1 class="font-extrabold text-5xl">What our customers say</h1>
       <div class="flex items-center justify-center text-center gap-6">
-        <div class="bg-menu p-2 cursor-pointer hover:bg-menu/60">
+        <div
+          class="bg-menu p-2 cursor-pointer hover:bg-menu/60"
+          @click="prevReview"
+        >
           <img :src="left" alt="<-" class="w-10" />
         </div>
-        <div class="bg-home w-full p-10 flex flex-col gap-6 text-white text-md">
-          <div>
-            <p class="text-2xl font-normal">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
-              perspiciatis labore doloribus accusantium voluptates aliquam
-              repudiandae ipsa voluptas laudantium! Sit?
-            </p>
-          </div>
-          <div class="flex flex-col gap-2">
-            <div class="flex items-center justify-center gap-2">
-              <img :src="star" class="w-4" />
-              <img :src="star" class="w-4" />
-              <img :src="star" class="w-4" />
-              <img :src="star" class="w-4" />
-              <img :src="star" class="w-4" />
+        <transition name="slide-fade" mode="out-in">
+          <div
+            key="currentIndex"
+            class="bg-home w-1/2 p-10 flex flex-col gap-6 text-white text-md"
+          >
+            <div>
+              <p class="text-2xl font-normal">
+                {{ reviews[currentIndex].text }}
+              </p>
             </div>
-            <div><p>CXV</p></div>
+            <div class="flex flex-col gap-2">
+              <div class="flex items-center justify-center gap-2">
+                <img
+                  v-for="i in reviews[currentIndex].rating"
+                  :key="i"
+                  :src="star"
+                  class="w-4"
+                />
+              </div>
+              <div>
+                <p>{{ reviews[currentIndex].author }}</p>
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="bg-menu p-2 cursor-pointer hover:bg-menu/60">
+        </transition>
+        <div
+          class="bg-menu p-2 cursor-pointer hover:bg-menu/60"
+          @click="nextReview"
+        >
           <img :src="right" alt="->" class="w-10" />
         </div>
       </div>
@@ -46,9 +58,36 @@ export default {
       star,
       right,
       left,
+      currentIndex: 0,
+      reviews: [
+        {
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, perspiciatis labore doloribus accusantium voluptates aliquam repudiandae ipsa voluptas laudantium! Sit?",
+          rating: 5,
+          author: "CXV",
+        },
+        {
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, perspiciatis labore doloribus accusantium voluptates aliquam repudiandae ipsa voluptas laudantium! Sit?",
+          rating: 4,
+          author: "John Doe",
+        },
+        {
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, perspiciatis labore doloribus accusantium voluptates aliquam repudiandae ipsa voluptas laudantium! Sit?",
+          rating: 3,
+          author: "Jane Smith",
+        },
+      ],
     };
+  },
+  methods: {
+    nextReview() {
+      this.currentIndex = (this.currentIndex + 1) % this.reviews.length;
+    },
+    prevReview() {
+      this.currentIndex =
+        (this.currentIndex - 1 + this.reviews.length) % this.reviews.length;
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
