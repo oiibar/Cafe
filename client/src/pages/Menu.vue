f4cfe7310495c5f52584db2579b6044f07d18e33
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-06-12 15:06:54 +0500

    cart

--- client/src/pages/Menu.vue
+++ client/src/pages/Menu.vue
@@ -1,4 +1,19 @@
 <template>
+  <div class="bg-home text-white p-6 px-10 flex justify-between items-center">
+    <div>
+      <a
+        class="flex items-center text-center cursor-pointer justify-center gap-2"
+        href=""
+      >
+        <p class="text-2xl font-bold">Caffein</p>
+        <img :src="logo" alt="Logo" class="w-10" />
+      </a>
+    </div>
+    <div class="relative">
+      <img :src="cart" alt="Cart" class="w-8 cursor-pointer" />
+      <div class="w-2 h-2 bg-[red] rounded-full absolute top-0 right-0"></div>
+    </div>
+  </div>
   <div
     id="menu"
     class="bg-menu py-20 px-6 sm:px-20 text-center flex flex-col justify-center items-center gap-16"
@@ -46,12 +61,16 @@
 <script>
 import CoffeeCard from "@/components/CoffeeCard.vue";
 import axios from "axios";
+import logo from "@/assets/logo.svg";
+import cart from "@/assets/cart.svg";
 
 export default {
   components: { CoffeeCard },
   data() {
     return {
       menu: [],
+      logo,
+      cart,
       isLoading: true,
     };
   },
